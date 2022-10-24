d997c79f25ac52645150e804d0cc562e787d2b84
Sylvester Wong <sylvester@theSylvester.ca>
2022-10-24 15:20:55 -0400

    Create solution.js

---
+++ 14-days-study-plan-to-crack-algo-i/35-search-insert-position/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length;
+    
+    while (low < high) {
+        let mid = Math.floor((low+high)/2);
+        if (nums[mid] === target)
+            return mid;
+        if (nums[mid] > target)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
+};
